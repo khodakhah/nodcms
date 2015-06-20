32f50d50f1394c2431ddfb99e37e4aa1da127b3f
khodakhah <khodakhah.mojtaba@yahoo.com>
2015-06-20 20:57:49 +0200

    Update nodcms.sql

--- install/installer/masks/nodcms.sql
+++ install/installer/masks/nodcms.sql
@@ -1,17 +1,3 @@
-/*
-Navicat MySQL Data Transfer
-
-Source Server         : localhost_3306
-Source Server Version : 50527
-Source Host           : localhost:3306
-Source Database       : abc
-
-Target Server Type    : MYSQL
-Target Server Version : 50527
-File Encoding         : 65001
-
-Date: 2013-08-21 15:56:11
-*/
 
 SET FOREIGN_KEY_CHECKS=0;
 
