f08d8d0597ba01a9aed37d0db89ed5b7d7b0c1c5
Mojtaba <khodakhah.mojtaba@yahoo.com>
2019-05-22 15:19:11 +0200

    Solve display modal title problem.

--- assets/nodcms/js/common.js
+++ assets/nodcms/js/common.js
@@ -34,8 +34,8 @@ function translate(value){
             '<div class="modal-dialog '+modal_class+'" role="document">'+
             '<div class="modal-content">'+
             '<div class="modal-header">'+
-            '<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>'+
             '<h4 class="modal-title">'+title+'</h4>'+
+            '<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>'+
             '</div>'+
             '<div class="modal-body">'+
             '</div>'+
