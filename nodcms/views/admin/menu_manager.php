e7b92f57fab9b05805e09bb0e943ec822bf636e5
Mojtaba <khodakhah.mojtaba@yahoo.com>
2019-05-24 17:39:02 +0200

    Remove unnecessary Modal codes.

--- nodcms/views/admin/menu_manager.php
+++ nodcms/views/admin/menu_manager.php
@@ -63,18 +63,6 @@
         <?php } ?>
     <?php } ?>
 </div>
-<div class="modal fade" id="menu-form" tabindex="-1" role="dialog">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title">Modal title</h4>
-            </div>
-            <div class="modal-body">
-            </div>
-        </div><!-- /.modal-content -->
-    </div><!-- /.modal-dialog -->
-</div><!-- /.modal -->
 
 <?php $this->load->addCssFile("assets/plugins/jquery-nestable/jquery.nestable"); ?>
 <?php $this->load->addJsFile("assets/plugins/jquery-nestable/jquery.nestable"); ?>
