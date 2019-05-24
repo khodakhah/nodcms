ff5661b273e2059a1e8f12afe0540ef66f8f4b4c
Mojtaba <khodakhah.mojtaba@yahoo.com>
2019-05-24 17:39:41 +0200

    Solve postfix display of currency field issue.

--- nodcms/views/common/form/bootstrap/currency.php
+++ nodcms/views/common/form/bootstrap/currency.php
@@ -4,7 +4,9 @@
     <?php } ?>
     <input data-divider="<?php echo $divider?>" data-target="#<?php echo $field_id; ?>" id="<?php echo $field_id; ?>-formatted" class="form-control currency-format <?php echo $class; ?>" type="text" value="<?php echo $default; ?>" <?php foreach ($attr as $key=>$value){ echo $key.' = "'.$value.'"'; } ?>>
     <?php if(isset($after_sign) && $after_sign!=""){ ?>
-        <span class="input-group-addon"><?php echo $after_sign; ?></span>
+        <div class="input-group-prepend">
+            <div class="input-group-text"><?php echo $after_sign; ?></div>
+        </div>
     <?php } ?>
 </div>
 <input name="<?php echo $name; ?>" id="<?php echo $field_id; ?>" type="hidden" value="<?php echo $default; ?>">
