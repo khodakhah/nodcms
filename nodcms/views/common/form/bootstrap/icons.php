bb418bbf7f1848e26d211e555a17a09c8681f560
Mojtaba <khodakhah.mojtaba@yahoo.com>
2019-06-20 12:28:29 +0200

    Debug icons select.

--- nodcms/views/common/form/bootstrap/icons.php
+++ nodcms/views/common/form/bootstrap/icons.php
@@ -1,7 +1,7 @@
 <div class="input-group input-large">
     <div class="input-group">
         <div class="input-group-prepend">
-            <div class="input-group-text"><i id="btn_icon_<?php echo $field_id; ?>" class="<?echo $default?>"></i></div>
+            <div class="input-group-text"><i id="btn_icon_<?php echo $field_id; ?>" class="<?php echo $default; ?>"></i></div>
         </div>
         <input onkeyup="$('#btn_icon_<?php echo $field_id; ?>').attr('class',$(this).val());" name="<?php echo $name; ?>" id="<?php echo $field_id; ?>" class="form-control <?php echo $class; ?>" type="text" value="<?php echo $default; ?>" <?php foreach ($attr as $key=>$value){ echo $key.' = "'.$value.'"'; } ?>>
         <div class="btn-group-prepend">
