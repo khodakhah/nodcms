260d347733887defb1bea210ad756e2140f99526
Mojtaba <khodakhah.mojtaba@yahoo.com>
2021-03-26 21:17:53 +0100

    Debug login user module problem.

--- nodcms-users/Config/View.php
+++ nodcms-users/Config/View.php
@@ -2,19 +2,21 @@
 
 class View extends \Config\View
 {
-	/**
-         * NodCMS variable!
-         * Path of view file. This path will attached before view files with
-         *
-         * @var string
+    /**
+     * @inheritdoc
+     * @var string
      */
     public $namespacePieces = 'NodCMS\Users';
 
     /**
-     * NodCMS variable!
-     * Path of view namespace for layout view file.
-     *
+     * @inheritdoc
      * @var string
      */
     public $namespaceLayout = 'NodCMS\Users';
+
+    /**
+     * @inheritdoc
+     * @var string
+     */
+    public $frameFile = 'layout';
 }
