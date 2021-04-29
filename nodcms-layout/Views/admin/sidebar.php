b5bf1b2e7e6666fd23eec27973a78ced82a07d83
Mojtaba <khodakhah.mojtaba@yahoo.com>
2021-04-29 21:24:07 +0200

    Debug empty sidebar menu with sub links.

--- nodcms-layout/Views/admin/sidebar.php
+++ nodcms-layout/Views/admin/sidebar.php
@@ -1,5 +1,5 @@
 <li class="nav-item" id="<?php echo $key; ?>">
-    <a class="nav-link nav-toggle" href="<?php echo $uri; ?>">
+    <a class="nav-link nav-toggle" href="<?php echo $uri === null ? "javascript:;" : $uri; ?>">
         <i class="<?php echo $icon; ?>"></i>
         <span class="title"><?php echo $title; ?></span>
         <?php if(!empty($subLink)){ ?>
