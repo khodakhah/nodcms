a6a90eb7c324db7a3fadcdf494e59f110f89c1d2
Mojtaba <khodakhah.mojtaba@yahoo.com>
2021-05-27 23:17:02 +0200

    Set the Membership (the parent of membership controllers).

--- nodcms-core/Controllers/Membership.php
+++ nodcms-core/Controllers/Membership.php
@@ -21,7 +21,44 @@
 
 namespace NodCMS\Core\Controllers;
 
+use Config\Models;
+use Config\Services;
+use Config\ViewMembership;
+
 abstract class Membership extends App
 {
+    public function __construct()
+    {
+        parent::__construct();
+        Services::layout()->setConfig(new ViewMembership());
+        Services::sidebar()->setConfig(new ViewMembership());
 
+        $this->userdata = Services::identity()->getUserData();
+
+        // Load language of a user
+        $this->language = Models::languages()->getOne($this->userdata['language_id']);
+        // Set language from database
+        Services::language()->set($this->language);
+
+        // Load settings from DB
+        Services::settings()->load($this->language['language_id']);
+        $this->settings = Services::settings()->get();
+
+        Services::sidebar()->addLink(
+            'dashboard',
+            _l("Dashboard", $this),
+            base_url('user/dashboard'),
+            'fas fa-th-large'
+        );
+
+        Services::sidebar()->addLink(
+            'dashboard',
+            _l("Account", $this),
+            base_url('user/account'),
+            'fas fa-user-cog'
+        );
+
+        // Call membership of startup of all modules
+        Services::modules()->executeMembership();
+    }
 }
