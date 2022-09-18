dcb916fbf9b356fe8e2f4e5b8fc29862abb41a30
Mojtaba Khodakhah <info@nodcms.com>
2022-09-18 18:03:28 +0200

    Add new composer script to fix code styles.

---
+++ php-cs-fixer.dist.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ *  This file is part of nodcms.
+ *
+ *  (c) Mojtaba Khodakhah <info@nodcms.com>
+ *  https://nodcms.com
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ *
+ */
+
+$rules = [
+  '@PSR12' => true,
+];
+
+$finder = PhpCsFixer\Finder::create()
+  ->in(__DIR__)
+  ->exclude('vendor')
+  ->exclude('writable')
+;
+
+$config = new PhpCsFixer\Config();
+return $config
+  ->setRules($rules)
+  ->setFinder($finder)
+  ->setUsingCache(false)
+;
