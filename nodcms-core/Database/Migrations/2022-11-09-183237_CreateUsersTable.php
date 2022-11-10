110b1fea8d88b8572524871a77486e79f13e0187
anvari182 <anvari182@gmail.com>
2022-11-10 20:38:39 +0100

    Add database seeder and migration

---
+++ nodcms-core/Database/Migrations/2022-11-09-183237_CreateUsersTable.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace NodCMS\Core\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateUsersTable extends Migration
+{
+    public function up()
+    {
+        //
+    }
+
+    public function down()
+    {
+        //
+    }
+}
