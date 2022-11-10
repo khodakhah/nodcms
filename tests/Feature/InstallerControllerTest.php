5b0e0d7ff3bf69c98a03b6643d37ed19f4330760
anvari182 <anvari182@gmail.com>
2022-11-10 21:04:37 +0100

    Refactor

--- tests/Feature/InstallerControllerTest.php
+++ tests/Feature/InstallerControllerTest.php
@@ -3,24 +3,36 @@
 namespace Tests\Feature;
 
 use CodeIgniter\Test\ControllerTestTrait;
-use CodeIgniter\Test\DatabaseTestTrait;
+use CodeIgniter\Test\TestResponse;
 use Exception;
 use NodCMS\Installer\Controllers\Installer;
 use Tests\TestCase;
 
 class InstallerControllerTest extends TestCase
 {
-    use DatabaseTestTrait;
     use ControllerTestTrait;
 
     /**
      * @test
      * @throws Exception
      */
-    public function testUserLogin()
+    public function installerStartTest()
     {
+        /** @var TestResponse $result */
         $result = $this->controller(Installer::class)->execute('start');
         $result->assertSee('Welcome');
+        $result->assertOk();
+    }
+
+    /**
+     * @test
+     * @throws Exception
+     */
+    public function installerLicenseTest()
+    {
+        /** @var TestResponse $result */
+        $result = $this->controller(Installer::class)->execute('license');
+        $result->assertSee('License Agreement');
         $result->assertOk();
     }
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,10 +6,4 @@ use CodeIgniter\Test\CIUnitTestCase;
 
 class TestCase extends CIUnitTestCase
 {
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        define('DB_CONFIG_PATH', '../'.getenv('app.dbConfigPath'));
-    }
 }
